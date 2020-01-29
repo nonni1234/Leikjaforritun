af4aa824aff5b9f2fedca7b8c790e8a2173271ea
Jón Bjarki Gíslason <jongi252@nemi.tskoli.is>
2020-01-29 12:28:30 +0000

    Byrjaði smá

---
+++ Verkefni2/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update() {
+        if (Input.GetKey("space")) {
+            rb.AddForce(0, 1000 * Time.deltaTime,0,ForceMode.JumpForce);
+        }
+    
+    }
+}
