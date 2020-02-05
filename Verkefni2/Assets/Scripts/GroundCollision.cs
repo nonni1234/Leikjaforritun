c3930fc498edc932f1989e4ff2233eb38e2e3b92
Jón Bjarki Gíslason <jongi252@nemi.tskoli.is>
2020-02-05 11:22:56 +0000

    skipti um tölvu

---
+++ Verkefni2/Assets/Scripts/GroundCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GroundCollision : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnCollisionEnter() {
+        player.Grounded();
+    }
+}
---
+++ Verkefni2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform player;
+    Rigidbody rb;
+    GameObject pl;
+    bool grounded;
+    public float Force;
+
+    public void Grounded() { // Runnar þegar player collidar við ground
+        grounded = true;
+        Debug.Log("Grounded");
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GetComponent<Transform>();
+    }
+    void Update() {
+
+        // Hreyfingin
+
+        if (Input.GetKey("space") && grounded == true) { // Að hoppa
+            grounded = false;
+            rb.AddForce(0, 500 * Time.deltaTime,0,ForceMode.Impulse);
+        }
+        // wasd movement
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, Force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -Force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-Force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(Force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
